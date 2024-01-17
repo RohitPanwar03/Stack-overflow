1ec68ad6b9e84df68b4126c08041291b049a608a
rohitpanwar1 <108470973+rohitpanwar1@users.noreply.github.com>
2024-01-17 19:14:06 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -23,13 +23,13 @@ app.use('/user', userrouter)
 app.use('/questions', questionrouter)
 app.use('/answer', answerRouter)
 
-const Port = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000
 
 const DATABASE = process.env.CONNECTION_URL
 
 mongoose.connect(DATABASE, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => app.listen(Port, () => {
-        console.log(`server running on port ${Port}`)
+    .then(() => app.listen(PORT, () => {
+        console.log(`server running on port ${PORT}`)
     })).catch((err) => {
         console.log(err.message)
     })
